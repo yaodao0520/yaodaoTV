56088bc3a00df1ce89f1f1197f70c164e7bdab13
yaodao0520 <yaodao456@gmail.com>
2025-05-15 17:56:11 +0800

    Update config.js

--- home/ubuntu/project/yaodaoTV-main/js/config.js
+++ home/ubuntu/project/yaodaoTV-main/js/config.js
@@ -15,7 +15,7 @@ const SITE_CONFIG = {
     name: 'yaodaoTV',
     url: 'https://libretv.is-an.org',
     description: '免费在线视频搜索与观看平台',
-    yaodaoTVo: './image/retrotv_5520.png',
+    logo: './image/retrotv_5520.png',
     version: '1.0.3'
 };
 
